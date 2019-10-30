18e484c97aa70806f9520c3d91d2ad05867b56b6
A Vidmar <a.v.vidmar@gmail.com>
2019-10-30 19:16:10 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var h = window.innerHeight;
+var w = window.innerWidth;
+
+// var docLang = document.documentElement.lang;
+// var header = document.getElementById('header');
+// var headerHero = document.querySelector('.header__hero');
+// var headerGreen_h = headerGreen.offsetHeight;
+
+console.log('hello world!');
