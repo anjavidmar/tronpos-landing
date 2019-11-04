c3fbe79cbc586151463c6fae2b16e1c740a6fffb
A Vidmar <a.v.vidmar@gmail.com>
2019-11-04 13:02:58 +0100

    desktop styling added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 var h = window.innerHeight;
 var w = window.innerWidth;
 
-// var docLang = document.documentElement.lang;
-// var header = document.getElementById('header');
-// var headerHero = document.querySelector('.header__hero');
-// var headerGreen_h = headerGreen.offsetHeight;
-
 console.log('hello world!');
+console.log('screen height = ' + h, 'screen width = ' + w);
+
+
+// var tagline = document.getElementsByTagName('p')[0];
+// var tagline_text = '<span class="text--blue-background">Davčna blagajna, ki vam pohitri poslovni proces</span><br /><span class="text--blue-background">za 30% brez dodatnega dela iz vaše strani.</span><br /><span class="text--blue-background">Sedaj že samo za <span class="text--yellow">29,90&euro;</span>/mesec.</span>'
+
+// console.log(tagline);
+
+// if (w > 800) {
+//     tagline.innerHTML = tagline_text;
+// }
